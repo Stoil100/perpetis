40dfa818899cbd2eb64b198c0d4cfb073c5fdcaf
Stoil100 <toli100ilov@gmail.com>
2024-10-07 23:20:58 +0300

    Chore: Add navigation and footer to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Footer from "@/components/footer";
+import Navigation from "@/components/navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-helvetica`}
       >
+        <Navigation/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
