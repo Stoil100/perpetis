97c63980e583baa8337a8d534df1348e95cca236
Stoil100 <toli100ilov@gmail.com>
2024-10-07 23:19:59 +0300

    Feat: Create footer design

---
+++ src/components/footer.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="relative text-white">
+            <div className="absolute inset-0 h-full w-full bg-[url('/background.png')] bg-cover bg-center" />
+            <div className="relative grid grid-cols-2 grid-rows-2 gap-4 bg-[#063971]/95 px-6 py-6 md:grid-cols-4 md:px-40 md:py-8">
+                <div className="col-span- md:col-span-2 row-span-2 space-y-4 flex flex-col justify-between">
+                    <Image
+                        src="/logo.png"
+                        width={150}
+                        height={150}
+                        alt="Logo"
+                    />
+                    <div className="text-sm sm:text-base md:text-lg">
+                        <p>Твоят сервиз за ремонт на</p>
+                        <p>бяла електротехника</p>
+                    </div>
+                </div>
+                <div className="col-span-1 col-start-2 row-start-1 flex-wrap font-bold flex justify-between gap-4 md:col-span-2 md:col-start-3 md:row-start-1">
+                    <a
+                        href="#"
+                        className="text-sm hover:underline sm:text-base md:text-lg"
+                    >
+                        Начало
+                    </a>
+                    <a
+                        href="#"
+                        className="text-sm hover:underline sm:text-base md:text-lg"
+                    >
+                        Галерия
+                    </a>
+                    <a
+                        href="#"
+                        className="text-sm hover:underline sm:text-base md:text-lg"
+                    >
+                        Локация
+                    </a>
+                    <a
+                        href="#"
+                        className="text-sm hover:underline sm:text-base md:text-lg"
+                    >
+                        Контакт
+                    </a>
+                </div>
+                <div className="col-span-1 md:col-span-2 row-span-1 justify-end col-start-2 row-start-2 flex flex-col space-y-2 md:col-start-3 md:row-start-2 md:flex-row md:items-end md:justify-between">
+                    <div className="text-sm sm:text-base md:text-lg">
+                        <p>Локация</p>
+                        <p>Ул. Свещаров 10, Варна</p>
+                        <p>Васил Костадинов</p>
+                    </div>
+                    <p className="text-sm sm:text-base md:text-lg">
+                        &copy; {new Date().getFullYear()} - Copyright All Rights
+                        Reserved
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+}
